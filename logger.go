276b3c94745a417617b7088914ca52066ae57a17
Daniel Holczer <holczer.dani95@gmail.com>
2025-06-01 16:12:38 +0200

    major update, with new functions

--- logger.go
+++ logger.go
@@ -1,19 +1,91 @@
 package logger
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"time"
+)
 
-func Info(data interface{}) {
-	fmt.Println("Log info: ", data)
+const (
+	LogError   = "ERROR"
+	LogWarning = "WARNING"
+	LogInfo    = "INFO"
+	LogDebug   = "DEBUG"
+)
+
+func Info(data interface{}, mode string) {
+	b, err := checkMode(mode)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if b {
+		fmt.Println(data)
+	} else {
+		logWriter(LogInfo, data)
+	}
+
 }
 
-func Error(data interface{}) {
-	fmt.Println("Log error: ", data)
+func Error(data interface{}, mode string) {
+	b, err := checkMode(mode)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if b {
+		fmt.Println("Log error: ", data)
+	} else {
+		logWriter(LogError, data)
+	}
 }
 
-func Warning(data interface{}) {
-	fmt.Println("Log warning: ", data)
+func Warning(data interface{}, mode string) {
+	b, err := checkMode(mode)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if b {
+		fmt.Println("Log warning: ", data)
+	} else {
+		logWriter(LogWarning, data)
+	}
 }
 
-func Debug(data interface{}) {
-	fmt.Println("Log debug: ", data)
+func Debug(data interface{}, mode string) {
+	b, err := checkMode(mode)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if b {
+		fmt.Println("Log debug: ", data)
+	} else {
+		logWriter(LogDebug, data)
+	}
+}
+
+func checkMode(mode string) (bool, error) {
+	switch mode {
+	case "stdout":
+		return true, nil
+	case "file":
+		return false, nil
+	default:
+		return false, fmt.Errorf("Unsupported mode: %s", mode)
+	}
+}
+
+func logWriter(level, data interface{}) {
+	file, err := os.OpenFile("log.txt", os.O_APPEND|os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	outData := fmt.Sprintf("[%s] %v [%s]\n", level, data, time.Now().Format("2006-01-02 15:04:05"))
+	file.WriteString(outData)
+
+	defer file.Close()
 }
